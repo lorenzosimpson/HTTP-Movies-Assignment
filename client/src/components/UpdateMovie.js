f5b1a3f644c3018a7157712202cf269fecc39986
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-17 17:05:07 -0400

    put request returning updated values

--- client/src/components/UpdateMovie.js
+++ client/src/components/UpdateMovie.js
@@ -16,15 +16,32 @@ const UpdateMovie = props => {
 
         if (movieToEdit) setMovie(movieToEdit)
 
-    }, [])
+    }, [props.movies, props.match.params.id])
 
-    
+    const handleChanges = e => {
+        setMovie({
+            ...movie,
+            [e.target.name]: e.target.value
+        })
+    }
 
+    const submitForm = e => {
+        e.preventDefault();
+        axios
+        .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+        .then(res => {
+            console.log(res)
+            setMovie(res.data)
+            // clean this up later
+        })
+
+    }
+console.log(movie)
     return (
-        <form>
-            <input type='text' name='title'></input>
-            <input type='text' name='director'></input>
-            <input type='number' name='metascore'></input>
+        <form onSubmit={submitForm}>
+            <input type='text' name='title' value={movie.title} onChange={handleChanges}></input>
+            <input type='text' name='director' value={movie.director} onChange={handleChanges}></input>
+            <input type='number' name='metascore' value={movie.metascore} onChange={handleChanges}></input>
             <button>Update Movie</button>
         </form>
     )
